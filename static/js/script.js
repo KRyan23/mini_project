4ff2303658df1da1a27e7e63ed805d1c666c519f
KRyan23 <GitHub@kryan23.bulc.club>
2021-09-28 11:35:22 +0000

    bind data from db to accordian

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
-
-
+// For acordian 
 $(document).ready(function(){
     $('.collapsible').collapsible();
   });
+// For Tooltips
+$(document).ready(function(){
+  $('.tooltipped').tooltip();
+});
